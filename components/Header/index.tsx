47bc3a436143889b3220f2c1dbda8eec1eaed970
kemal <kemaltaufikf@gmail.com>
2025-02-04 23:22:46 +0700

    fix style

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -55,58 +55,60 @@ export default function Header({ opened, toggle, handleLogout }: Props) {
             }}
           />
 
-          <Flex align="center" gap={8}>
-            <Menu width={200} position="bottom-end">
-              <Menu.Target>
-                <Flex align="center" gap={10}>
-                  <Flex pos="relative">
-                    <Avatar size={34} src="/placeholder-image.jpg" alt="it's me" />
-                    <Box
-                      w={12}
-                      h={12}
-                      bg="green.4"
-                      style={{
-                        borderRadius: '50%',
-                        border: '2px solid black',
-                      }}
-                      pos="absolute"
-                      bottom={-2}
-                      right={-2}
-                    />
+          <Group>
+            <Flex align="center" gap={8}>
+              <Menu width={200} position="bottom-end">
+                <Menu.Target>
+                  <Flex align="center" gap={10}>
+                    <Flex pos="relative">
+                      <Avatar size={34} src="/placeholder-image.jpg" alt="it's me" />
+                      <Box
+                        w={12}
+                        h={12}
+                        bg="green.4"
+                        style={{
+                          borderRadius: '50%',
+                          border: '2px solid black',
+                        }}
+                        pos="absolute"
+                        bottom={-2}
+                        right={-2}
+                      />
+                    </Flex>
+                    <Text className={classes.username} lts={-0.4} fz={12} fw={700}>
+                      Shin Thant
+                    </Text>
                   </Flex>
-                  <Text className={classes.username} lts={-0.4} fz={12} fw={700}>
-                    Shin Thant
-                  </Text>
-                </Flex>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item onClick={() => {}}>Profile</Menu.Item>
-                <Menu.Item onClick={handleLogout}>Logout</Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Flex>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item onClick={() => {}}>Profile</Menu.Item>
+                  <Menu.Item onClick={handleLogout}>Logout</Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Flex>
 
-          <Flex align="center" gap={8}>
-            <Tooltip label="Create Transaction" withArrow position="top">
-              <ActionIcon
-                onClick={openAddTransactionModal}
-                size="lg"
-                variant="filled"
-                radius="100%"
-              >
-                <IconPlus size={20} />
+            <Flex align="center" gap={8}>
+              <Tooltip label="Create Transaction" withArrow position="top">
+                <ActionIcon
+                  onClick={openAddTransactionModal}
+                  size="lg"
+                  variant="filled"
+                  radius="100%"
+                >
+                  <IconPlus size={20} />
+                </ActionIcon>
+              </Tooltip>
+            </Flex>
+
+            <Flex align="center" gap={8}>
+              <ActionIcon className={classes.actionIcon} size={30} radius="xl" variant="subtle">
+                <IconNotification size={16} />
               </ActionIcon>
-            </Tooltip>
-          </Flex>
-
-          <Flex align="center" gap={8}>
-            <ActionIcon className={classes.actionIcon} size={30} radius="xl" variant="subtle">
-              <IconNotification size={16} />
-            </ActionIcon>
-            <ActionIcon className={classes.actionIcon} size={30} radius="xl" variant="subtle">
-              <IconSettings size={16} />
-            </ActionIcon>
-          </Flex>
+              <ActionIcon className={classes.actionIcon} size={30} radius="xl" variant="subtle">
+                <IconSettings size={16} />
+              </ActionIcon>
+            </Flex>
+          </Group>
         </Flex>
       </Group>
       <Divider visibleFrom="md" w="100%" />
